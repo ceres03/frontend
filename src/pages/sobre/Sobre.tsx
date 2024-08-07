dc7870b4b0cf71c959ff3acb42763552c0da891c
ouxnq <buenop.vinicius@gmail.com>
2024-08-07 14:24:13 -0300

    refactor: clean code

--- src/pages/sobre/Sobre.tsx
+++ src/pages/sobre/Sobre.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './Sobre.css';
 
 function Sobre() {
@@ -6,12 +5,12 @@ function Sobre() {
         <div className="sobre-nos">
             <h1>Sobre nós</h1>
             <p>
-            Um aplicativo voltado para distribuição e aumento de visibilidade de produtores locais e produtos para agricultores. Preços descentralizados do mercado padrão (idealmente menores) voltados para a população vulnerável das cidades.
+                Um aplicativo voltado para distribuição e aumento de visibilidade de produtores locais e produtos para agricultores. Preços descentralizados do mercado padrão (idealmente menores) voltados para a população vulnerável das cidades.
             </p>
             <br></br>
             <h1>Noss objetivo</h1>
             <p>
-            Estimular a agricultura local, o estímulo para agriculturas orgânicas, evitar o desperdício de alimentos e garantir a qualidade dos mercados de commodities de alimentos.  
+                Estimular a agricultura local, o estímulo para agriculturas orgânicas, evitar o desperdício de alimentos e garantir a qualidade dos mercados de commodities de alimentos.
             </p>
             <h2>Informações de Contato</h2>
             <ul>
