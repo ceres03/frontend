bc71a1d7e781600af5f43a2df6d5c2e2528f8870
Julio <juliohcf@hotmail.com>
2024-08-07 14:13:12 -0300

    Refactor Navbar e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import "./index.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes></Routes>
-    </BrowserRouter>
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 export default App;
