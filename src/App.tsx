3cea913c021745c39f56dde4775bd07be6199cb1
Rojinhas <ruthrojasdasilva12@gmail.com>
2024-08-13 14:41:37 -0300

    Componente Login Log

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./index.css";
 import Footer from "./components/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./pages/login/Login";
 import Sobre from "./pages/sobre/Sobre";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Cadastro from "./pages/Cadastro/Cadastro";
 import { AuthProvider } from "./contexts/AuthContext";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
