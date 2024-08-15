31f08a08cd679ea03d687f600b9c801727fe8805
Julio <juliohcf@hotmail.com>
2024-08-15 16:39:26 -0300

    home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import homevideo from "../../assets/homevideonew.mp4";
-import logo from "../../assets/logo2.png";
-import back from "../../assets/back.png";
+import homevideo from "../../assets/Homevideonew.mp4";
+import logo from "../../assets/logo.png";
+import back from "../../assets/";
 import { motion } from "framer-motion";
 
 function Home() {
