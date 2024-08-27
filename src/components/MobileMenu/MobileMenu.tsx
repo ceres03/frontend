1c6b9a038ebe6a206115ac430f2d9a67236f5e1b
sarassaura <sarahyukinonakada@gmail.com>
2024-08-27 17:13:03 -0300

    fix: mobile menu close behaviour
    responsive footer and home

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Spin as Hamburger } from "hamburger-react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./MobileMenu.css";
 
 function MobileMenu({
@@ -14,6 +14,38 @@ function MobileMenu({
 }) {
   const [isOpen, setOpen] = useState(false);
   const [fadeAnimation, setFadeAnimation] = useState(false);
+  const menuRef = useRef(null);
+  const buttonRef = useRef(null);
+
+  const navigate = useNavigate();
+
+  const closeMenu = (url: string) => {
+    setFadeAnimation(false);
+    navigate(url);
+  };
+
+  const handleOutsideClick = (event) => {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(event.target) &&
+      event.target !== buttonRef.current
+    ) {
+      setFadeAnimation(false);
+    }
+  };
+
+  const handleResize = () => {
+    setFadeAnimation(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     if (fadeAnimation) {
@@ -31,29 +63,31 @@ function MobileMenu({
         className="xl:hidden block text-5xl cursor-pointer"
         onClick={() => setFadeAnimation(!fadeAnimation)}
         type="button"
+        ref={buttonRef}
       >
-        <Hamburger />
+        <Hamburger toggled={isOpen} toggle={setOpen} />
       </button>
       <nav
         className={`${fadeAnimation && "fade-in"} mobile__menu ${
           isOpen && "open "
         } `}
         style={{ transition: "transform 0.5s ease, opacity 0.5s ease" }}
+        ref={menuRef}
       >
         <div className="w-full container">
           {links.map((link) => (
             <li key={link.name}>
-              <Link key={link.name} to={link.url}>
+              <a key={link.name} onClick={() => closeMenu(link.url)}>
                 {link.name}
-              </Link>
+              </a>
             </li>
           ))}
           {admin &&
             adminLinks.map((link) => (
               <li key={link.name}>
-                <Link key={link.name} to={link.url}>
+                <a key={link.name} onClick={() => closeMenu(link.url)}>
                   {link.name}
-                </Link>
+                </a>
               </li>
             ))}
         </div>
